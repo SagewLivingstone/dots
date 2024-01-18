a5cd9b1357698c79de69e0064a42219d60f9bfac
Sage <sagewlivingstone@gmail.com>
2024-01-17 20:18:31 -0700

    install pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+  ],
   runtimeConfig: {
     apiSecret: '1234',
+  },
+  pinia: {
+    storesDirs: ['./stores/**']
   }
 })
